3f348eb7fe0f60bc2c2d995e1302dd8f946c6f5d
Ben McClelland <ben.mcclelland@versity.com>
2015-05-07 13:57:28 -0700

    update build system

--- GNUmakefile
+++ GNUmakefile
@@ -21,7 +21,7 @@
 #
 #	You should have received a copy of the GNU Lesser General Public
 #	License along with this library; if not, write to the Free Software
-#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 DEPTH = .
 
@@ -32,7 +32,7 @@ BUILD_64BIT = yes
 #   all is defined here so it is the default.
 .PHONY: all install
 
-DIRS = 3rdparty
+DIRS = 3rdparty src
 
 install: all
 
--- mk/targets.mk
+++ mk/targets.mk
@@ -2,35 +2,26 @@
 #
 #      VSM - Versity Storage Management File System
 #
-#               Copyright (c) 2007 Sun Microsystems, Inc. All Rights Reserved.
-#               Copyright (c) 2015 Versity, Inc. All Rights Reserved.
-#
-#               U.S. Government Rights - Commercial software. Government users are
-#       subject to the Sun Microsystems, Inc. standard license agreement and applicable
-#       provisions of the FAR and its supplements.
-#
-#       Use is subject to license terms. Sun, Sun Microsystems and the Sun logo
-#       are trademarks or registered trademarks of Sun Microsystems, Inc. in the U.S.
-#       and other countries.
+#		Copyright (c) 2015 Versity, Inc. All Rights Reserved.
 #
 #    VSM_notice_end
 #
 #
-#       LGPL NOTICE
+#	LGPL NOTICE
 #
-#       This library is free software; you can redistribute it and/or
-#       modify it under the terms of the GNU Lesser General Public
-#       License as published by the Free Software Foundation; either
-#       version 2.1 of the License, or (at your option) any later version.
+#	This library is free software; you can redistribute it and/or
+#	modify it under the terms of the GNU Lesser General Public
+#	License as published by the Free Software Foundation; either
+#	version 2.1 of the License, or (at your option) any later version.
 #
-#       This library is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#       Lesser General Public License for more details.
+#	This library is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	Lesser General Public License for more details.
 #
-#       You should have received a copy of the GNU Lesser General Public
-#       License along with this library; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#	You should have received a copy of the GNU Lesser General Public
+#	License along with this library; if not, write to the Free Software
+#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 #	targets.mk - target definition
 #
